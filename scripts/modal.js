a14aa4f269f1cd5957610281030219445a04baf2
WitchDevelops <d.n.wojewska@gmail.com>
2025-06-13 19:36:45 +0200

    refactor: modal handling, autoplay, no-scroll when modal is open

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,58 +1,27 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Modal = exports.modalData = void 0;
-exports.modalData = {
-    title: "Video Title",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-};
-var Modal = /** @class */ (function () {
-    function Modal() {
-        this.dialog = this.createDialog();
-        this.videoContainer = this.dialog.querySelector("#video-container");
-        document.body.appendChild(this.dialog);
+// scripts/modal.ts
+var dialog = document.querySelector("#video-dialog");
+var videoContainer = document.querySelector(".gallery__item--video-container");
+var iframe = dialog.querySelector("iframe");
+var closeModalButton = dialog.querySelector(".close-dialog");
+var videoSrc = "https://www.youtube.com/embed/x6iyz1AQhuU?si=GaHSWBFI3dra_mtF";
+var autoplaySrc = "".concat(videoSrc, "&autoplay=1");
+videoContainer === null || videoContainer === void 0 ? void 0 : videoContainer.addEventListener("click", function () {
+    if (!dialog.open) {
+        iframe.src = autoplaySrc;
+        dialog.showModal();
+        document.body.classList.add("no-scroll");
     }
-    Modal.prototype.createDialog = function () {
-        var dialog = document.createElement("dialog");
-        dialog.id = "video-dialog";
-        dialog.className = "dialog";
-        dialog.setAttribute("aria-modal", "true");
-        dialog.setAttribute("role", "dialog");
-        dialog.innerHTML = "\n      <div class=\"dialog-content\">\n        <div class=\"dialog-content__header\">\n          ".concat(exports.modalData.title
-            ? "<h2 class=\"dialog-content__title\">".concat(exports.modalData.title, "</h2>")
-            : "", "\n          <button type=\"button\" class=\"dialog-content__button close-dialog btn btn-danger btn-close\" aria-label=\"Close video\">\n            <i class=\"fa-solid fa-xmark\"></i>\n          </button>\n        </div>\n        ").concat(exports.modalData.text
-            ? "<p class=\"dialog-content__text\">".concat(exports.modalData.text, "</p>")
-            : "", "\n        <div id=\"video-container\" class=\"dialog-content__video--container\"></div>\n      </div>");
-        return dialog;
-    };
-    Modal.prototype.openModal = function (videoUrl) {
-        var embedUrl = this.convertToEmbedUrl(videoUrl);
-        this.videoContainer.innerHTML = this.createVideoIframe(embedUrl);
-        this.dialog.showModal();
-    };
-    Modal.prototype.closeModal = function () {
-        this.videoContainer.innerHTML = "";
-        this.dialog.close();
-    };
-    Modal.prototype.createVideoIframe = function (embedUrl) {
-        return "\n      <iframe\n        src=\"".concat(embedUrl, "?autoplay=1\"\n        frameborder=\"0\"\n        allow=\"autoplay; encrypted-media\"\n        allowfullscreen\n        title=\"YouTube video\"\n        class=\"dialog-content__video--video\">\n      </iframe>");
-    };
-    Modal.prototype.convertToEmbedUrl = function (url) {
-        var match = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)/);
-        return match ? "https://www.youtube.com/embed/".concat(match[1]) : "";
-    };
-    return Modal;
-}());
-exports.Modal = Modal;
-document.addEventListener("DOMContentLoaded", function () {
-    var modal = new Modal();
-    var videoButton = document.querySelector(".gallery__item--video-btn");
-    videoButton === null || videoButton === void 0 ? void 0 : videoButton.addEventListener("click", function (event) {
-        var button = event.target;
-        var videoUrl = button.getAttribute("data-video-url") || "";
-        modal.openModal(videoUrl);
-    });
-    var closeButton = document.querySelector(".close-dialog");
-    closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener("click", function () {
-        modal.closeModal();
-    });
+});
+closeModalButton === null || closeModalButton === void 0 ? void 0 : closeModalButton.addEventListener("click", function () {
+    dialog.close();
+});
+window.addEventListener("click", function (event) {
+    if (event.target === dialog) {
+        dialog.close();
+    }
+});
+dialog.addEventListener("close", function () {
+    document.body.classList.remove("no-scroll");
+    iframe.src = "";
+    setTimeout(function () { return (iframe.src = videoSrc); }, 100);
 });
--- scripts/modal.ts
+++ scripts/modal.ts
@@ -1,89 +1,36 @@
-export const modalData = {
-  title: "Video Title",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-};
+// scripts/modal.ts
 
-export class Modal {
-  private dialog: HTMLDialogElement;
-  private videoContainer: HTMLDivElement;
+const dialog = document.querySelector("#video-dialog") as HTMLDialogElement;
+const videoContainer = document.querySelector(
+  ".gallery__item--video-container"
+) as HTMLElement;
+const iframe = dialog.querySelector("iframe") as HTMLIFrameElement;
+const closeModalButton = dialog.querySelector(".close-dialog") as HTMLElement;
 
-  constructor() {
-    this.dialog = this.createDialog();
-    this.videoContainer = this.dialog.querySelector(
-      "#video-container"
-    ) as HTMLDivElement;
-    document.body.appendChild(this.dialog);
+const videoSrc =
+  "https://www.youtube.com/embed/x6iyz1AQhuU?si=GaHSWBFI3dra_mtF";
+const autoplaySrc = `${videoSrc}&autoplay=1`;
+
+videoContainer?.addEventListener("click", () => {
+  if (!dialog.open) {
+    iframe.src = autoplaySrc;
+    dialog.showModal();
+    document.body.classList.add("no-scroll");
   }
+});
 
-  private createDialog(): HTMLDialogElement {
-    const dialog = document.createElement("dialog");
-    dialog.id = "video-dialog";
-    dialog.className = "dialog";
-    dialog.setAttribute("aria-modal", "true");
-    dialog.setAttribute("role", "dialog");
-    dialog.innerHTML = `
-      <div class="dialog-content">
-        <div class="dialog-content__header">
-          ${
-            modalData.title
-              ? `<h2 class="dialog-content__title">${modalData.title}</h2>`
-              : ""
-          }
-          <button type="button" class="dialog-content__button close-dialog btn btn-danger btn-close" aria-label="Close video">
-            <i class="fa-solid fa-xmark"></i>
-          </button>
-        </div>
-        ${
-          modalData.text
-            ? `<p class="dialog-content__text">${modalData.text}</p>`
-            : ""
-        }
-        <div id="video-container" class="dialog-content__video--container"></div>
-      </div>`;
-    return dialog;
+closeModalButton?.addEventListener("click", () => {
+  dialog.close();
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === dialog) {
+    dialog.close();
   }
+});
 
-  public openModal(videoUrl: string): void {
-    const embedUrl = this.convertToEmbedUrl(videoUrl);
-    this.videoContainer.innerHTML = this.createVideoIframe(embedUrl);
-    this.dialog.showModal();
-  }
-
-  public closeModal(): void {
-    this.videoContainer.innerHTML = "";
-    this.dialog.close();
-  }
-
-  private createVideoIframe(embedUrl: string): string {
-    return `
-      <iframe
-        src="${embedUrl}?autoplay=1"
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="YouTube video"
-        class="dialog-content__video--video">
-      </iframe>`;
-  }
-
-  private convertToEmbedUrl(url: string): string {
-    const match = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)/);
-    return match ? `https://www.youtube.com/embed/${match[1]}` : "";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = new Modal();
-  const videoButton = document.querySelector(".gallery__item--video-btn");
-
-  videoButton?.addEventListener("click", (event) => {
-    const button = event.target as HTMLButtonElement;
-    const videoUrl = button.getAttribute("data-video-url") || "";
-    modal.openModal(videoUrl);
-  });
-
-  const closeButton = document.querySelector(".close-dialog");
-  closeButton?.addEventListener("click", () => {
-    modal.closeModal();
-  });
+dialog.addEventListener("close", () => {
+  document.body.classList.remove("no-scroll");
+  iframe.src = "";
+  setTimeout(() => (iframe.src = videoSrc), 100);
 });
